96eb16ac9b3e9fe361060e9fc1507f1f8fdb04b3
Alok <aloksivam@gmail.com>
2024-10-03 00:00:50 +0530

    understood promise, then, async await

--- 21Promises/01promise_intro.js
+++ 21Promises/01promise_intro.js
@@ -51,45 +51,44 @@
 // }).finally(()=>console.log("finally, promise is either resolved or rejected"))
 // // console.log(name2)
 
-// const promiseFive =  new Promise(function(resolve,reject){
-//     setTimeout(function(){
-//         let error = true;
-//         if(!error){
-//             resolve({username:"javascript",password:123})
-//         }
-//         else{
-//             reject('Error:JS went wrong')
-//         }
-//     },1000)
-// });
+const promiseFive =  new Promise(function(resolve,reject){
+    setTimeout(function(){
+        let error = true;
+        if(error){
+            resolve({username:"javascript",password:123})
+        }
+        else{
+            reject('Error:JS went wrong')
+        }
+    },1000) 
+});
 
-// async function consumePromiseFive(){
-//     try {
-        
-//         const response = await promiseFive
-//         console.log(response)
-//     } catch (error) {
-//         console.log("error");
-//     }
-// }
-
-// consumePromiseFive()
-
-async function getAllUsers() {
+async function consumePromiseFive(){
     try {
-        const response = await fetch('https://jsonplaceholder.typicode.com/users')
-    const data = response.json() // it also takes time so you need to make it await too
-    console.log(data)
-    // console.log(response)
-    } 
-    catch (error) {
-        console.log("E: ",error);
+        const response = await promiseFive
+        console.log(response)
+    } catch (error) {
+        console.log("error");
     }
 }
 
-// getAllUsers()
+consumePromiseFive()
 
-// fetch('https://jsonplaceholder.typicode.com/users').then((response)=>{return response.json()}).then((data)=>{console.log(data)})
+// async function getAllUsers() {
+//     try {
+//         const response = await fetch('https://jsonplaceholder.typicode.com/users')
+//     const data = response.json() // it also takes time so you need to make it await too
+//     console.log(data)
+//     // console.log(response)
+//     } 
+//     catch (error) {
+//         console.log("E: ",error);
+//     }
+// }
+
+// // getAllUsers()
+
+// // fetch('https://jsonplaceholder.typicode.com/users').then((response)=>{return response.json()}).then((data)=>{console.log(data)})
+// // .catch((error)=>console.log(response))
+// fetch('https://api.github.com/users/Aloksivam').then((response)=>{return response.json()}).then((data)=>{console.log(data)})
 // .catch((error)=>console.log(response))
-fetch('https://api.github.com/users/Aloksivam').then((response)=>{return response.json()}).then((data)=>{console.log(data)})
-.catch((error)=>console.log(response))
