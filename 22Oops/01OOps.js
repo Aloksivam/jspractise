90615866c03aa8a2bfe6e86017e11c0b8699b0ae
Alok <aloksivam@gmail.com>
2024-10-03 01:36:45 +0530

    started understanding about how oops work in js which is nothing but a prototype based language

---
+++ 22Oops/01OOps.js
@@ -0,0 +1,10 @@
+//js does not support oops it is basically a prototype based language, its classes are primarily syntactic sugar over existing prototype based inheritance mechanisms.
+
+//Object literal = data type used to define objects in js.
+const user = {
+    name:"alok",
+    age:21,
+    getintro:function intro(){
+        console.log("hi, i am a method present inside an object")
+    }
+}
