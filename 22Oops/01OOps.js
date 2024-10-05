6b912b1082bcaeca426938450811bff455025db3
Alok <aloksivam@gmail.com>
2024-10-06 00:00:56 +0530

    understandint about prototype

--- 22Oops/01OOps.js
+++ 22Oops/01OOps.js
@@ -1,23 +1,25 @@
 //js does not support oops it is basically a prototype based language, its classes are primarily syntactic sugar over existing prototype based inheritance mechanisms.
-
 //Object literal = data type used to define objects in js.
 const user = {
     name:"alok",
-    age:21,
+   logincount:2,
+   isLoggedin:true,
     getintro:function intro(){
         console.log(this.name)
+        // console.log(this)
     }
 }
-user.getintro();
+// user.getintro();
 // //constructor function
 // let date = new Date();
 // const promiseone  = new Promise();
-// function user(username, logincount,isLoggedin){
-//     this.username = username;
-//     this.logincount = logincount;
-//     this.isLoggedin = isLoggedin;
-//     return this
-// }
-// let userone = new user("alok",12,true);
-// let usertwo = new user("shivam",15,false);
-// console.log(userone.isLoggedin)
+function User(username, logincount,isLoggedin){
+    this.username = username;
+    this.logincount = logincount;
+    this.isLoggedin = isLoggedin;
+    return this
+}
+let userone = new User("alok",12,true);
+let usertwo = new User("shivam",15,false);
+console.log(userone.isLoggedin)
+console.log(userone.constructor())
